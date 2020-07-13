f9bd6022a5dc85028c4c6268d57214cfd2cfb268
nilput <nilputs@gmail.com>
2020-07-13 11:12:46 +0000

    added a benchmark

---
+++ examples/bench/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm results.txt
+echo 'Make sure you compiled knit with "make opt -B"!'
+perf stat -r 100 php7.2     gcd.php 2>> results.txt
+perf stat -r 100 python3    gcd.py  2>> results.txt
+perf stat -r 100 lua5.3     gcd.lua 2>> results.txt
+perf stat -r 100 ../../knit gcd.kn  2>> results.txt
+
